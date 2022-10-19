f50a93261a79ce7038c094cdc08c22bb00a2e25e
Pedro Rondelli <pedrorondelli@hotmail.com>
2022-10-19 18:16:16 -0300

    implements the static part of the registration page

--- src/Registration.js
+++ src/Registration.js
@@ -1,3 +1,90 @@
+import styled from "styled-components";
+import logo from "./loginImage.png";
+
 export default function Registration() {
-  return <>Registration</>;
+  return (
+    <RegistrationScreen>
+      <div>
+        <img src={logo} alt="TrackIt" />
+        <form>
+          <input placeholder="email" />
+          <input placeholder="senha" />
+          <input placeholder="nome" />
+          <input placeholder="foto" />
+          <button>Cadastrar</button>
+        </form>
+        <p>Já tem uma conta? Faça login!</p>
+      </div>
+    </RegistrationScreen>
+  );
 }
+
+const RegistrationScreen = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  div {
+    width: 90%;
+    height: 85vh;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  input {
+    width: 100%;
+    height: 45px;
+    margin: 5px 0;
+
+    padding: 5px;
+    border-radius: 5px;
+    box-sizing: border-box;
+
+    ::placeholder {
+      font-family: "Lexend Deca", sans-serif;
+      font-size: 19.98px;
+      font-weight: 400;
+      line-height: 25px;
+      letter-spacing: 0em;
+      color: #c4bebe;
+    }
+  }
+  button {
+    padding: 5px;
+
+    font-family: Lexend Deca;
+    font-size: 21px;
+    line-height: 26px;
+    letter-spacing: 0em;
+    text-align: center;
+
+    width: 100%;
+
+    background-color: #52b6ff;
+
+    color: white;
+
+    border-radius: 5px;
+  }
+  p {
+    color: #52b6ff;
+    text-decoration-line: underline;
+    font-family: Lexend Deca;
+    font-size: 14px;
+    line-height: 17px;
+    letter-spacing: 0em;
+
+    cursor: pointer;
+  }
+  img {
+    margin-bottom: 40px;
+  }
+
+  form {
+    margin-bottom: 20px;
+  }
+`;
